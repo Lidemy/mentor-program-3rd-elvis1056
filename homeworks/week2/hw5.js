fce4c7b00e3cb1d561ab7c9fda4f7b4e4b952bb3
elvislinreallusion <elvislin@reallusion.com>
2019-05-10 10:01:47 +0800

    'week2.hw'

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,21 @@
 function join(str, concatStr) {
-  return concatStr;
+  let ans = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (i === str.length - 1) {
+      ans += str[i];
+    } else {
+      ans = ans + str[i] + concatStr;
+    }
+  }
+  return ans;
 }
 
 function repeat(str, times) {
-  return times;
+  let result = '';
+  for (let i = 0; i < times; i += 1) {
+    result += str;
+  }
+  return result;
 }
 
 console.log(join('a', '!'));
