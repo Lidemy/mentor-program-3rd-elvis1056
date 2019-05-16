c09141a193fe68d91e1dd8d95b903611ed99f437
elvis1056 <john800116@hotmail.com>
2019-05-17 00:03:12 +0800

    week3.hw

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,17 @@
+function makestar(i) {
+  let ans = '';
+  for (let j = 1; j <= i; j += 1) {
+    ans += '*';
+  }
+  return ans;
+}
+
 function stars(n) {
-  console.log(n);
+  const result = [];
+  for (let i = 1; i <= n; i += 1) {
+    result.push(makestar(i));
+  }
+  return result;
 }
 
 module.exports = stars;
--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,13 @@
+function reverse(str) {
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    result += str[i];
+  }
+  return result;
+}
+
 function isPalindromes(str) {
-  console.log(str);
+  return str === reverse(str);
 }
 
 module.exports = isPalindromes;
